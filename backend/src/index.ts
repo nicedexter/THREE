ea3791ae1ecfbc1cd1302258e285a78597c42225
Manuel Spuhler <manuel.spuhler@chuv.ch>
2024-05-02 16:38:04 +0200

    update dev script in backend/package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+const app = express()
+const fs = require("fs")
+const YAML = require('yaml')
+const swaggerUi = require('swagger-ui-express')
+
+const file = fs.readFileSync('./api.yml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker!');
-});
+  res.send('Hello from Docker !')
+})
 
-const port = process.env.PORT || 4000;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+  console.log(`Server started on port ${port}`)
+})
